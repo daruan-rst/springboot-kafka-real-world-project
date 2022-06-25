09e54b9a683b1b91ada3d41bac22d83ef7739a63
daruan <daruan.rst@gmail.com>
2022-06-24 21:13:15 -0300

    Saving Wikimedia Data into Database

--- kafka-consumer-database/src/main/java/net/javaguides/springboot/KafkaDatabaseConsumer.java
+++ kafka-consumer-database/src/main/java/net/javaguides/springboot/KafkaDatabaseConsumer.java
@@ -1,5 +1,7 @@
 package net.javaguides.springboot;
 
+import net.javaguides.springboot.entity.WikimediaData;
+import net.javaguides.springboot.repository.WikimediaDataRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,11 +12,21 @@ public class KafkaDatabaseConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
 
+    public KafkaDatabaseConsumer(WikimediaDataRepository dataRepository) {
+        this.dataRepository = dataRepository;
+    }
+
+    private WikimediaDataRepository dataRepository;
+
     @KafkaListener(
             topics = "wikimedia_recentchange",
             groupId = "myGroup"
     )
     public void consumer(String eventMessage){
         LOGGER.info(String.format("Event message received -> %s" , eventMessage));
+
+        WikimediaData data = new WikimediaData();
+        data.setWikiEventData(eventMessage);
+        dataRepository.save(data);
     }
 }
